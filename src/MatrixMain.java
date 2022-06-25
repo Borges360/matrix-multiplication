1392e84b35e928677b0ae9f1c5529af86de9b24d
Luiz Felipe <felipeborgeseng@outlook.com.br>
2022-06-25 17:58:56 -0300

    Correcting Threads implementation for matrix multiplication V3

--- src/MatrixMain.java
+++ src/MatrixMain.java
@@ -4,17 +4,17 @@ public class MatrixMain {
 
     private static int[][] matrix2;
 
-    private static final int matrixSize = 1024;
+    private static final int matrixSize = 64;
 
     public static void main(String[] args) {
 
         createMatrixs();
 
         ExecuteMatrix executeMatrix = new ExecuteMatrix();
-        executeMatrix.averageV1(matrix1, matrix2, matrixSize);
-        executeMatrix.averageV2(matrix1, matrix2, matrixSize);
-        executeMatrix.averageV3(matrix1, matrix2, matrixSize);
-//        executeMatrix.compareMatrixs(executeMatrix.averageV2(matrix1, matrix2, matrixSize), executeMatrix.averageV3(matrix1, matrix2, matrixSize), matrixSize);
+//        executeMatrix.averageV1(matrix1, matrix2, matrixSize);
+//        executeMatrix.averageV2(matrix1, matrix2, matrixSize);
+//        executeMatrix.averageV3(matrix1, matrix2, matrixSize);
+        System.out.println(executeMatrix.compareMatrixs(executeMatrix.averageV2(matrix1, matrix2, matrixSize), executeMatrix.averageV3(matrix1, matrix2, matrixSize), matrixSize));
 
 
     }
--- src/MatrixMultiplicationV3.java
+++ src/MatrixMultiplicationV3.java
@@ -1,24 +1,41 @@
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MatrixMultiplicationV3 {
 
-    public synchronized int[][] calculate(int[][] matriz1, int [][] matriz2, int matrixSize) {
+    public synchronized int[][] calculate(int[][] matriz1, int[][] matriz2, int matrixSize) {
+        final int[][] matrix = new int[matrixSize][matrixSize];
 
-        int[][] matrix = new int[matrixSize][matrixSize];
-
+        CountDownLatch latchA = new CountDownLatch((int) (Math.pow(matrixSize, 3)));
         AtomicInteger i = new AtomicInteger();
-        for(i.get(); i.get() < matrixSize; i.getAndIncrement()){
-            new Thread(() -> {
-                for(AtomicInteger k = new AtomicInteger(); k.get() < matrixSize; k.getAndIncrement()){
-                    new Thread(() -> {
-                        for(AtomicInteger j = new AtomicInteger(); j.get() < matrixSize; j.getAndIncrement()){
-                            new Thread(() -> {
-                                matrix[i.get()][j.get()] = matrix[i.get()][j.get()] + matriz1[i.get()][k.get()] * matriz2[k.get()][j.get()];
+        for (i.get(); i.get() < matrixSize; i.getAndIncrement()) {
+            final int newI = i.get();
+            Thread thread1 = new Thread(() -> {
+
+                AtomicInteger k = new AtomicInteger();
+                for (k.get(); k.get() < matrixSize; k.getAndIncrement()) {
+                    final int newk = k.get();
+                    Thread thread2 = new Thread(() -> {
+
+                        AtomicInteger j = new AtomicInteger();
+                        for (j.get(); j.get() < matrixSize; j.getAndIncrement()) {
+                            final int newj = j.get();
+                            Thread thread3 = new Thread(() -> {
+                                matrix[newI][newj] = matrix[newI][newj] + matriz1[newI][newk] * matriz2[newk][newj];
+                                latchA.countDown();
                             });
+                            thread3.start();
                         }
                     });
+                    thread2.start();
                 }
             });
+            thread1.start();
+        }
+        try {
+            latchA.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return matrix;
     }
