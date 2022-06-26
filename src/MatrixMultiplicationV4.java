755898a8971ab07619c3ad1545a33477e60a3db6
Luiz Felipe <felipeborgeseng@outlook.com.br>
2022-06-26 01:46:23 -0300

    Simplify Version 4 Matrix Multiplication

--- src/MatrixMultiplicationV4.java
+++ src/MatrixMultiplicationV4.java
@@ -5,34 +5,30 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MatrixMultiplicationV4 {
 
-    public int[][] calculate(int[][] matriz1, int[][] matriz2, int matrixSize) {
+    public synchronized int[][] calculate(int[][] matriz1, int[][] matriz2, int matrixSize) {
         final int[][] matrix = new int[matrixSize][matrixSize];
         CountDownLatch latchA = new CountDownLatch((int) (Math.pow(matrixSize, 3)));
-        AtomicInteger ih = new AtomicInteger();
+
         int partitionSize = matrixSize / 4;
         List<Thread> threadsIh = new ArrayList<>();
-        for (ih.get(); ih.get() < matrixSize; ih.getAndAdd(partitionSize)) {
-            final int newIh = ih.get();
+        for (int ih = 0; ih < matrixSize; ih+= partitionSize) {
+            final int newIh = ih;
             Thread threadIh = new Thread(() -> {
                 List<Thread> threadsJh = new ArrayList<>();
-                AtomicInteger jh = new AtomicInteger();
-                for (jh.get(); jh.get() < matrixSize; jh.getAndAdd(partitionSize)) {
-                    final int newJh = jh.get();
+
+                for (int jh = 0; jh < matrixSize; jh+= partitionSize) {
+                    final int newJh = jh;
                     Thread threadJh = new Thread(() -> {
 
-                        AtomicInteger kh = new AtomicInteger();
-                        for(kh.get(); kh.get() < matrixSize; kh.getAndAdd(partitionSize)){
-                            final int newKh = kh.get();
-                            AtomicInteger il = new AtomicInteger();
-                            for(il.get(); il.get() < partitionSize; il.getAndIncrement()){
-                                final int newIl = il.get();
-                                AtomicInteger kl = new AtomicInteger();
-                                for(kl.get(); kl.get() < partitionSize; kl.getAndIncrement()){
-                                    final int newKl = kl.get();
-                                    AtomicInteger jl = new AtomicInteger();
-                                    for(jl.get(); jl.get() < partitionSize; jl.getAndIncrement()){
-                                        final int newJl = jl.get();
-                                        matrix[newIh+newIl][newJh+newJl] += matriz1[newIh+newIl][newKh+newKl] * matriz2[newKh+newKl][newJh+newJl];
+                        for(int kh = 0; kh < matrixSize; kh+= partitionSize){
+                            
+                            for(int il = 0; il < partitionSize; il++){
+                                
+                                for(int kl = 0; kl < partitionSize; kl++){
+                                    
+                                    for(int jl = 0; jl < partitionSize; jl++){
+                                        
+                                        matrix[newIh+il][newJh+jl] += matriz1[newIh+il][kh+kl] * matriz2[kh+kl][newJh+jl];
                                         latchA.countDown();
                                     }
                                 }
