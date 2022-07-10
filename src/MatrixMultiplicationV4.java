cd65bc347105d0617dfef65be160a92a601f454b
Luiz Felipe <felipeborgeseng@outlook.com.br>
2022-07-10 20:56:23 -0300

    correcting Algorithm 4

--- src/MatrixMultiplicationV4.java
+++ src/MatrixMultiplicationV4.java
@@ -1,15 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class MatrixMultiplicationV4 {
 
-    public synchronized int[][] calculate(int[][] matriz1, int[][] matriz2, int matrixSize) {
+    public int[][] calculate(int[][] matriz1, int[][] matriz2, int matrixSize) {
         final int[][] matrix = new int[matrixSize][matrixSize];
-        CountDownLatch latchA = new CountDownLatch((int) (Math.pow(matrixSize, 3)));
 
-        int partitionSize = matrixSize / 4;
+        int partitionSize = matrixSize / 16;
         List<Thread> threadsIh = new ArrayList<>();
         for (int ih = 0; ih < matrixSize; ih+= partitionSize) {
             final int newIh = ih;
@@ -27,9 +24,7 @@ public class MatrixMultiplicationV4 {
                                 for(int kl = 0; kl < partitionSize; kl++){
                                     
                                     for(int jl = 0; jl < partitionSize; jl++){
-                                        
-                                        matrix[newIh+il][newJh+jl] += matriz1[newIh+il][kh+kl] * matriz2[kh+kl][newJh+jl];
-                                        latchA.countDown();
+                                        matrix[newIh+il][newJh+jl] = matrix[newIh+il][newJh+jl] + matriz1[newIh+il][kh+kl] * matriz2[kh+kl][newJh+jl];
                                     }
                                 }
                             }
@@ -38,21 +33,16 @@ public class MatrixMultiplicationV4 {
                     });
                     threadJh.start();
                     threadsJh.add(threadJh);
-                    if (threadsJh.size() % 10 == 0) {
+                    if (threadsJh.size() % 16 == 0) {
                         waitForThreads(threadsJh);
                     }
                 }
             });
             threadIh.start();
             threadsIh.add(threadIh);
-            if (threadsIh.size() % 10 == 0) {
+            if (threadsIh.size() % 16 == 0) {
                 waitForThreads(threadsIh);
             }
-        }
-        try {
-            latchA.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
         return matrix;
     }
